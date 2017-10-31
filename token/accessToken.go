3c2c6468c28c5cb36c421045d60a13e2a475b586
RedWoodPi <272617278@qq.com>
2017-10-31 16:23:51 +0800

    fix

--- token/accessToken.go
+++ token/accessToken.go
@@ -1,1 +1,78 @@
 package token
+
+import (
+	"strings"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"bytes"
+	"encoding/json"
+	"database/sql"
+	_"github.com/Go-SQL-Driver/MySQL"
+)
+
+const appid = "wxccc67a998a00e936"
+const secret = "rmE9eObzaupBLM5aQ8xkGYpCVsuFYZA6IhmG9IEQ7sE"
+
+
+//获取accessToken
+func AccessToken(appid string, secret string) (string, error) {
+
+	type AccessTokenResponse struct{
+		AccessToken string   `json:"access_token"`
+		ExpireIn    float64  `json:"expire_in"`
+	}
+
+	//appid := "wxccc67a998a00e936"
+	//secret := "rmE9eObzaupBLM5aQ8xkGYpCVsuFYZA6IhmG9IEQ7sE"
+	url := strings.Join([]string{"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=",
+		appid,
+			"&secret=",
+				secret},"")
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("获取accessToken错误", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if bytes.Contains(body, []byte("access_token")){
+		atr := AccessTokenResponse{}
+		err = json.Unmarshal(body, &atr)
+
+		if err != nil {
+			fmt.Println("解析返回json失败")
+			return "", err
+		}
+		return atr.AccessToken, nil
+	}else{
+		fmt.Println("wrong")
+		return "", nil
+	}
+}
+
+//存储token
+func SaveToken() {
+	fmt.Print("begining to get accesstoken")
+	token, err := AccessToken(appid, secret)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	db ,err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/mysite?charset=utf8")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result, err := db.Exec("INSERT INTO accessToken (accessToken) VALUES (?)", token)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	_ = result
+
+	db.Close()
+}
