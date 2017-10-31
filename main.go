e4d9ec2e957e3438f42658d0d693a001f18315d7
RedWoodPi <272617278@qq.com>
2017-10-31 16:54:19 +0800

    add go

--- main.go
+++ main.go
@@ -10,16 +10,18 @@ import (
 
 //主程序
 func main()  {
+	go timeTike()
 	http.HandleFunc("/check", controllers.CheckSignature)
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal("Listenandserver: ", err)
 	}
+}
+
+func timeTike(){
 	token.SaveToken()
 	for range time.Tick(7200 * time.Second){
 		token.SaveToken()
 	}
-
 }
-
 
